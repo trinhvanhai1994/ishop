55b66c1b006a994ba4192ea40da079158ad7554b
HaiTv <haitv@ominext.com>
2020-05-18 18:57:13 +0700

    refactor

--- ishop-application/src/main/java/com/ominext/cms/response/CartResponse.java
+++ ishop-application/src/main/java/com/ominext/cms/response/CartResponse.java
@@ -1,6 +1,5 @@
 package com.ominext.cms.response;
 
-import com.ominext.cms.model.Product;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,7 +8,7 @@ import java.util.List;
 @Data
 public class CartResponse {
     private Long userId;
-    private List<Product> items;
+    private List<ItemResponse> items;
     private BigDecimal amountTotal;
     private BigDecimal amountAfterDiscountTotal;
 }
